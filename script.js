b8adbeda42f5e9153cf00ba28cfa0adb5f3542a0
Gustavo Martins <tioben@outlook.com.br>
2023-08-22 15:41:03 -0300

    22/08/2023

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const checkboxMenu = document.getElementById("menu-check");
 const menu = document.getElementById("menu");
 
+menu.style.right = -(menu.offsetWidth) + "px";
 checkboxMenu.addEventListener("change", function() {
+    menu.style.display = "flex";
     if (checkboxMenu.checked) {
-        menu.style.display = "flex";
+        menu.style.right = 0;
     } else {
-        menu.style.display = "none";
+        menu.style.right = -(menu.offsetWidth) + "px";
     }
 });
 
